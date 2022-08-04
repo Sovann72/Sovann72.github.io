1bcada45e32e71382ca98785a01e760e7e9fa0ad
sovannen72 <sovannen72@gmail.com>
2022-08-04 21:23:53 +0700

    fixed container overflowed

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -8,25 +8,31 @@ const htmlLogoStyle = htmlLogo.style;
 const cssLogoStyle = cssLogo.style;
 const jsLogoStyle = jsLogo.style;
 
+// skillset logo animation begin
 function beginSkillsWrapperDefaultAnimation(){
+  // htmlLogoStyle.display = 'none';
+  // cssLogoStyle.display = 'none';
+  // jsLogoStyle.display = 'none';
   htmlLogoStyle.transform = 'translateX(-100px)';
   cssLogoStyle.transform = 'translateY(100px)';
   jsLogoStyle.transform = 'translateX(100px)';
   return (htmlLogoStyle && cssLogoStyle && jsLogoStyle);
 }
+
+// skillset logo animation begin
 function finishedSkillsWrapperDefaultAnimation(){
+  // htmlLogoStyle.display = 'block';
+  // cssLogoStyle.display = 'block';
+  // jsLogoStyle.display = 'block';
   htmlLogoStyle.transform = 'translateX(0)';
   cssLogoStyle.transform = 'translateY(0)';
   jsLogoStyle.transform = 'translateX(0)';
   return (htmlLogoStyle && cssLogoStyle && jsLogoStyle);
 }
 
-
-
-
+// when user scroll let skillset logo come together
 window.addEventListener('load', ()=>{
   finishedSkillsWrapperDefaultAnimation();
-
     if (innerWidth < 680){
       beginSkillsWrapperDefaultAnimation();
       if(innerHeight > skillsWrapper.offsetTop){
